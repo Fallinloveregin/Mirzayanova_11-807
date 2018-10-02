74f9d50dd5f1bab7dd0dd748f3a72bc4a9f0921a
Regina <43280309+Fallinloveregin@users.noreply.github.com>
2018-10-02 12:01:49 +0300

    Add files via upload

--- farida/src/HW803.java
+++ farida/src/HW803.java
@@ -1,2 +1,42 @@
-public class HW803 {
+import java.util.Scanner;
+public class Homework3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //1 2 3
+        //8 9 4
+        //7 6 5
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int c = n * m, i = 0, j = -1;
+        int[][] matrix = new int[n][m];
+        int k = 0;
+        while (k < c) {
+            while (j + 1 < m && matrix[i][j + 1] == 0) {
+                j++;
+                k++;
+                matrix[i][j] = k;
+            }
+            while (i + 1 < n && matrix[i + 1][j] == 0) {
+                i++;
+                k++;
+                matrix[i][j] = k;
+            }
+            while (j - 1 > -1 && matrix[i][j - 1] == 0) {
+                j--;
+                k++;
+                matrix[i][j] = k;
+            }
+            while (i - 1 > -1 && matrix[i - 1][j] == 0) {
+                i--;
+                k++;
+                matrix[i][j] = k;
+            }
+        }
+        for (i = 0; i < n; i++) {
+            System.out.println();
+            for (j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+        }
+    }
 }
